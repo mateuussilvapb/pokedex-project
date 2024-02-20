18cc8afd1f3dd6b8635dd749fad1fc0d7c5e6a89
Mateus Dias <mateuussilvapb@gmail.com>
2024-02-20 15:21:02 -0300

    Feature: Componente de Loading, Poke-List, Poke-Search, Poke-Header e tela home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './pages/home/home.component';
+import { PokeHeaderComponent } from './components/poke-header/poke-header.component';
+import { PokeSearchComponent } from './components/poke-search/poke-search.component';
+import { LoadingComponentComponent } from './components/loading-component/loading-component.component';
+import { PokeListComponent } from './components/poke-list/poke-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PokeHeaderComponent,
+    PokeSearchComponent,
+    LoadingComponentComponent,
+    PokeListComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/loading-component/loading-component.component.ts
@@ -0,0 +1,9 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-loading-component',
+  templateUrl: './loading-component.component.html',
+  styleUrls: ['./loading-component.component.scss'],
+})
+export class LoadingComponentComponent {
+}
