3aae6c3c599534c203bb0cac98ac4c66ae65d7e0
Mateus Dias <mateuussilvapb@gmail.com>
2024-02-23 09:51:59 -0300

    Feature: Comentário.

--- src/environments/environment.localhost.ts
+++ src/environments/environment.localhost.ts
@@ -1,4 +1,8 @@
 export const environment = {
+  /**
+   * Informações sensíveis e deveriam estar em variáveis de ambientes.
+   * Porém, deixei aqui para facilitar os testes
+   */
   apiKey: 'ae20f6e0-a283-43f5-bf83-1ea1e0f91759',
   apiUrl: 'https://api.pokemontcg.io/v2',
 };
